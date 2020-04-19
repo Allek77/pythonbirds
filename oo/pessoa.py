fb3f686ddbbd88896d8d31135b7a5c31a436a8b8
4ll3K <Allek.vieira07>
2020-04-19 15:40:22 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos=2
+
     def __init__(self, *filhos, nome=None, idade=21):
       self.idade = idade
       self.nome = nome
@@ -20,5 +22,12 @@ if __name__ == '__main__':
         print(filho.nome)
     krahe.sobrenome = 'Vieira'
     del krahe.filhos
+    krahe.olhos = 1
+    del krahe.olhos
     print(krahe.__dict__)
     print(allek.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(krahe.olhos)
+    print(allek.olhos)
+    print(id(Pessoa.olhos), id(krahe.olhos), id(allek.olhos))
